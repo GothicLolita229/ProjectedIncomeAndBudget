cb6ac4a3e05b2cd2fc4147523c663c4d29550e55
GothicLolita229 <90853197+GothicLolita229@users.noreply.github.com>
2021-12-07 09:16:17 -0500

    Add files via upload

---
+++ NetBeansProjects/linaresFinalExam/src/linaresfinalexam/LinaresFinalExam.java
@@ -0,0 +1,58 @@
+package linaresfinalexam;
+
+/**
+ * Lourdes Linares
+ * 12.7.2021
+ * Final Exam
+ * Calculate gross pay for user
+ * 
+ */
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class LinaresFinalExam {
+
+   
+    public static void main(String[] args) 
+    {
+        Scanner k = new Scanner(System.in);
+        double rate = 0, hours = 0;
+        displayInfo();
+        rate = getRate(rate);
+        hours = getHours(hours);
+        double grossPay = calculateGrossPay(rate, hours);
+        displayRHGrossPay(rate, hours, grossPay);
+        
+    }
+    public static void displayInfo()
+    {
+        System.out.println("Lourdes Linares");
+        System.out.println("December 7th, 2021");
+        System.out.println("This program calculates the gross pay "
+                + "for an individual");
+    }
+    public static double getRate(double rate)
+    {
+        Scanner k = new Scanner(System.in);
+        System.out.print("Enter your pay rate: $");
+        rate = k.nextDouble();
+        return rate;
+    }
+    public static double getHours(double hours)
+    {
+        Scanner k = new Scanner(System.in);
+        System.out.print("Enter your hours worked: ");
+        hours = k.nextDouble();
+        return hours;
+    }
+    public static double calculateGrossPay(double rate, double hours)
+    {
+        return rate * hours;
+    }
+    public static void displayRHGrossPay(double rate, double hours, double grossPay)
+    {
+        DecimalFormat df = new DecimalFormat("#,###,###,##0.00");
+        System.out.println("The pay rate is $" + df.format(rate));
+        System.out.println("The hours worked are $" + df.format(hours));
+        System.out.println("The gross pay is $" + df.format(grossPay));
+    }
+}
